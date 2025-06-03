3851e613ffe3379371d662f56ec595278b0bc19d
secretindex <caio.smelo04@gmail.com>
2025-06-03 02:43:53 -0300

    create a little form template for later updates and implementation of backend comunication

--- app/contato/futura/page.tsx
+++ app/contato/futura/page.tsx
@@ -1,8 +1,40 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Mail } from "lucide-react"
+
 export default function FuturaContact () {
+  // axios
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center">
-      <div>
-        <h1>Futura Tecnologia</h1>
+      <div className="rounded-md flex flex-col justify-center items-center gap-2 p-4 w-1/4 border-[1px] border-[#0004]">
+        <h1 className="font-bold text-center text-2xl">Futura Tecnologia</h1>
+        <form action="post" className="w-full flex flex-col gap-3">
+          <div>
+            <label htmlFor="nome">Nome</label>
+            <Input id="nome" name="nome" type="text"/>
+          </div>
+          <div>
+            <label htmlFor="nome">Número</label>
+            <Input id="nome" name="numero" type="text"/>
+          </div>
+          <div>
+            <label htmlFor="nome">E-mail</label>
+            <Input id="email" name="email" type="email"/>
+          </div>
+          <div>
+            <label htmlFor="mensagem">Mensagem</label>
+            <Textarea id="mensagem" name="mensagem"/>
+          </div>
+          <div>
+            <Button className="w-full">
+              <Mail />
+              Enviar mensagem
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   )
