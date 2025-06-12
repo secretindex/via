16fdcc29bf4f85e20c6f671a7df808af8fe6aaa4
secretindex <caio.smelo04@gmail.com>
2025-06-12 17:22:24 -0300

    authentication feature

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <h1>
+          Login
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="w-full flex py-2">
+      <h1>VIA de Acesso</h1>
+    </div>
+  )
+}
+
+export default Header
